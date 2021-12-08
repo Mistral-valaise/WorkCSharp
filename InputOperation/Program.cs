4ef5b287c806f399242d6a6592f1c515fe29f3c0
mistral <nvalaise@outlook.de>
2021-12-08 21:32:29 +0100

    OperationProgram

---
+++ InputOperation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InputOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie Zahl1: ");
+            double value1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Geben Sie Zahl2: ");
+            double value2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(value1 + " + " + value2 + " = " + (value1 + value2));
+            Console.WriteLine(value1 + " - " + value2 + " = " + (value1 - value2));
+            Console.WriteLine(value1 + " x " + value2 + " = " + (value1 * value2));
+            Console.WriteLine(value1 + " : " + value2 + " = " + (value1 / value2));
+            Console.WriteLine(value1 + " % " + value2 + " = " + (value1 % value2));
+
+        }
+    }
+}
